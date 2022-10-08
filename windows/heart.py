a7da40f1f0ea4673a40f90965747a4b05edbab6e
hizellal <vladimir.liubshenko@gmail.com>
2022-10-08 18:33:05 +0300

    add instr.py and heart.py

---
+++ windows/heart.py
@@ -0,0 +1,23 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
+from instr import *
+
+class Win1(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+
+    def set_appear(self):
+        pass
+    def initUI(self):
+        pass
+    def connects(self):
+        pass
+
+app = QApplication([])
+win1 = Win1()
+
+app.exec_()
